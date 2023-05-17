b989aa8f404b05fc6dcee661909e6d341c7d5716
Kyle Rich <kylexrich@gmail.com>
2023-05-16 21:18:07 -0700

    added prompt templates

--- server/models/course.js
+++ server/models/course.js
@@ -6,6 +6,7 @@ const CourseSchema = new Schema({
   courseCode: { type: String, required: true },
   department: { type: String, required: true },
   school: { type: Schema.Types.ObjectId, ref: 'School' },
+  promptTemplates: [{ type: String }],
 });
 
 const Course = mongoose.model('Course', CourseSchema);
