3c75266de4fa38961a19181c727a7be6dbc47cb8
Kyle Rich <kylexrich@gmail.com>
2023-05-20 12:03:42 -0700

    tested all user endpoints with postman

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,17 +1,20 @@
 const getUser = (req, res) => {
-  // TODO
+  const userId = req.params.userId;
+  res.send({ data: `Hello get ${userId}` });
 };
 
 const createUser = (req, res) => {
-  res.send({ data: 'Hello World, from express' });
+  res.send({ data: `Hello post` });
 };
 
 const updateUser = (req, res) => {
-  // TODO
+  const userId = req.params.userId;
+  res.send({ data: `Hello put ${userId}` });
 };
 
 const deleteUser = (req, res) => {
-  // TODO
+  const userId = req.params.userId;
+  res.send({ data: `Hello delete ${userId}` });
 };
 
 module.exports = {
