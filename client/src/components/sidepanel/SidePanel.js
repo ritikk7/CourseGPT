6f86d54520b655b66c6a6ff5fcf8cd0c233ae901
Amy Jo <amyjojjm2001@gmail.com>
2023-05-23 23:33:06 -0700

    sidepanel layout

--- client/src/components/sidepanel/SidePanel.js
+++ client/src/components/sidepanel/SidePanel.js
@@ -1,17 +1,19 @@
 import React from 'react'
 import styles from './SidePanel.module.css';
-import { Select } from '@chakra-ui/react'
+import { Select, Button } from '@chakra-ui/react'
 
 const SidePanel = () => {
   return (
     <div className={styles.sidepanel}>
-        <Select placeholder='Select a course'>
+       <div className={styles.courseSelect}>
+       <Select placeholder='Select a course' _hover={{ bg: "rgb(61, 61, 61)" }}>
           <option value='cpsc455'>CPSC455</option>
           <option value='cpsc310'>CPSC310</option>  
           <option value='cpsc320'>CPSC320</option>
         </Select>
-        <div>New chat</div>
-        <div>Username</div> 
+        <Button mt={4} width='100%' bg='transparent' _hover={{ bg: "rgb(61, 61, 61)" }} border='1px' borderColor='gray.300'>+ New Chat</Button>
+       </div>
+        <div className={styles.profile}>Username</div> 
     </div>
   )
 }
