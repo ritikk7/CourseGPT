f56105133439301aaab77da69ece5f6e033e62a3
Amy Jo <amyjojjm2001@gmail.com>
2023-05-24 21:31:09 -0700

    prompts

--- client/src/components/right-section/RightSection.js
+++ client/src/components/right-section/RightSection.js
@@ -2,12 +2,17 @@ import React from 'react'
 import styles from './RightSection.module.css';
 import api from '../../api/axiosInstance';
 
-import {  Text  } from '@chakra-ui/react';
+import {  Text, HStack, Button  } from '@chakra-ui/react';
 
 import { ArrowForwardIcon } from '@chakra-ui/icons'
 
-
 const RightSection = () => {
+
+  const PromptButton = () => {
+    return (
+      <Button bg='#42434f' _hover={{ bg: "#2b2b2e" }} border='none' whiteSpace="normal" blockSize="auto" px={8} py={4}><Text fontSize='md' fontWeight={400}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor?</Text></Button>)
+  }
+
     const callApi = () => {
         api
           .post('/users')
@@ -23,6 +28,11 @@ const RightSection = () => {
     <div className={styles.container}>
       <div className={styles.mainPanel}>
        <Text as='b' fontSize='4xl'>CourseGPT</Text>
+<HStack mt={24} spacing='16px'>
+  <PromptButton/>
+  <PromptButton/>
+  <PromptButton/>
+</HStack>
       </div>
         <div className={styles.inputSection}>
            <div className={styles.inputArea}>
