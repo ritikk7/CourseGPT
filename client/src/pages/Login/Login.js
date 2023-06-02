dcd2429d85899527aaa51bd524c581598e1d6acd
Kyle Rich <kylexrich@gmail.com>
2023-06-01 21:14:03 -0700

    everything done, just need to add credits

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -1,18 +1,32 @@
-import React, {useEffect, useState} from 'react';
-import {Box, Button, Input, FormControl, FormLabel, Alert, AlertIcon} from '@chakra-ui/react';
-import {useDispatch, useSelector} from 'react-redux';
-import {loginUser} from '../../redux/authSlice';
-import {useNavigate} from 'react-router-dom';
-import {baseUrl} from "../../api/axiosInstance";
+import React, { useEffect, useState } from 'react';
+import {
+    Flex,
+    Box,
+    FormControl,
+    FormLabel,
+    Input,
+    Stack,
+    Link,
+    Button,
+    Heading,
+    Text,
+    Alert,
+    AlertIcon,
+} from '@chakra-ui/react';
+import { useDispatch, useSelector } from 'react-redux';
+import { loginUser } from '../../redux/authSlice';
+import { useNavigate } from 'react-router-dom';
+import { baseUrl } from "../../api/axiosInstance";
 
-function Login() {
+export default function Login() {
     const dispatch = useDispatch();
-    const [credentials, setCredentials] = useState({email: '', password: ''});
+    const [credentials, setCredentials] = useState({ email: '', password: '' });
     const navigate = useNavigate();
     const user = useSelector(state => state.auth.user);
     const error = useSelector(state => state.auth.error);
+
     const handleChange = (e) => {
-        setCredentials({...credentials, [e.target.name]: e.target.value});
+        setCredentials({ ...credentials, [e.target.name]: e.target.value });
     };
 
     const handleSubmit = (e) => {
@@ -30,52 +44,76 @@ function Login() {
     }, [user, navigate]);
 
     return (
-        <Box>
-            {error && (
-                <Alert status="error">
-                    <AlertIcon/>
-                    {error}
-                </Alert>
-            )}
-            <form onSubmit={handleSubmit}>
-                <FormControl id="email" isRequired>
-                    <FormLabel>Email</FormLabel>
-                    <Input
-                        name="email"
-                        type="text"
-                        placeholder="Email"
-                        onChange={handleChange}
-                    />
-                </FormControl>
-                <FormControl id="password" isRequired>
-                    <FormLabel>Password</FormLabel>
-                    <Input
-                        name="password"
-                        type="password"
-                        placeholder="Password"
-                        onChange={handleChange}
-                    />
-                </FormControl>
-                <Button mt={4} type="submit" colorScheme="teal">
-                    Login
-                </Button>
-            </form>
-            <Button
-                mt={4}
-                onClick={handleGoogleLogin}
-                colorScheme="red"
-            >
-                Login with Google
-            </Button>
-            <Button
-                mt={4}
-                onClick={navigateToRegister}
-                colorScheme="blue"
-            >
-                Don't have an account? Create one!
-            </Button>
-        </Box>
+        <Flex
+            minH={'100vh'}
+            align={'center'}
+            justify={'center'}
+            bg={'gray.800'}>
+            <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
+                <Stack align={'center'}>
+                    <Heading fontSize={'4xl'} color={'white'}>
+                        Sign in to your account
+                    </Heading>
+                </Stack>
+                <Box
+                    rounded={'lg'}
+                    bg={'gray.700'}
+                    boxShadow={'lg'}
+                    p={8}>
+                    {error && (
+                        <Alert status="error">
+                            <AlertIcon />
+                            {error}
+                        </Alert>
+                    )}
+                    <form onSubmit={handleSubmit}>
+                        <Stack spacing={4}>
+                            <FormControl id="email" isRequired>
+                                <FormLabel color={'white'}>Email address</FormLabel>
+                                <Input name="email" type="email" onChange={handleChange} />
+                            </FormControl>
+                            <FormControl id="password" isRequired>
+                                <FormLabel color={'white'}>Password</FormLabel>
+                                <Input name="password" type="password" onChange={handleChange} />
+                            </FormControl>
+                            <Stack spacing={2}>
+                                <Button
+                                    bg={'blue.600'}
+                                    color={'white'}
+                                    _hover={{
+                                        bg: 'blue.700',
+                                    }}
+                                    type="submit"
+                                >
+                                    Sign in
+                                </Button>
+                                <Text align='center' color={'white'}>
+                                    or
+                                </Text>
+                                <Button
+                                    bg={'red.600'}
+                                    color={'white'}
+                                    _hover={{
+                                        bg: 'red.700',
+                                    }}
+                                    onClick={handleGoogleLogin}
+                                >
+                                    Login with Google
+                                </Button>
+                            </Stack>
+                        </Stack>
+                    </form>
+                    <Flex justify="center" mt={4}>
+                        <Link onClick={navigateToRegister} fontSize='sm' color={'blue.400'}>
+                            Don't have an account? Create one!
+                        </Link>
+                    </Flex>
+                </Box>
+            </Stack>
+        </Flex>
     );
 }
 
-export default Login;
+
+
+// pulled this straight from https://chakra-templates.dev/templates/forms/authentication/simpleCard
--- client/src/pages/Register/Register.js
+++ client/src/pages/Register/Register.js
@@ -1,25 +1,41 @@
-import React, {useEffect, useState} from 'react';
-import {Box, Button, Input, FormControl, FormLabel, AlertIcon, Alert} from '@chakra-ui/react';
-import {useDispatch, useSelector} from 'react-redux';
-import {registerUser} from '../../redux/authSlice';
-import {useNavigate} from 'react-router-dom';
-import {baseUrl} from "../../api/axiosInstance";
+import React, { useEffect, useState } from 'react';
+import {
+    Flex,
+    Box,
+    FormControl,
+    FormLabel,
+    Input,
+    Stack,
+    Link,
+    Button,
+    Heading,
+    Text,
+    Alert,
+    AlertIcon,
+} from '@chakra-ui/react';
+import { useDispatch, useSelector } from 'react-redux';
+import { registerUser } from '../../redux/authSlice';
+import { useNavigate } from 'react-router-dom';
+import { baseUrl } from "../../api/axiosInstance";
 
-function Register() {
+export default function Register() {
     const dispatch = useDispatch();
-    const [credentials, setCredentials] = useState({email: '', password: '', firstName: '', lastName: ''});
+    const [credentials, setCredentials] = useState({ email: '', password: '', firstName: '', lastName: '' });
     const navigate = useNavigate();
     const user = useSelector(state => state.auth.user);
     const error = useSelector(state => state.auth.error);
 
     const handleChange = (e) => {
-        setCredentials({...credentials, [e.target.name]: e.target.value});
+        setCredentials({ ...credentials, [e.target.name]: e.target.value });
     };
 
     const handleSubmit = (e) => {
         e.preventDefault();
         dispatch(registerUser(credentials));
-    }
+    };
+
+    const handleGoogleLogin = () => (window.location.href = baseUrl + '/auth/google');
+    const navigateToLogin = () => navigate('/login');
 
     useEffect(() => {
         if (user) {
@@ -27,49 +43,81 @@ function Register() {
         }
     }, [user, navigate]);
 
-    const handleGoogleLogin = () => window.location.href = baseUrl + '/auth/google';
-    const navigateToLogin = () => navigate('/login');
-
     return (
-        <Box>
-            {error && (
-                <Alert status="error">
-                    <AlertIcon/>
-                    {error}
-                </Alert>
-            )}
-            <form onSubmit={handleSubmit}>
-                <FormControl id="firstName" isRequired>
-                    <FormLabel>First Name</FormLabel>
-                    <Input name="firstName" type="text" placeholder="First Name" onChange={handleChange}/>
-                </FormControl>
-                <FormControl id="lastName" isRequired>
-                    <FormLabel>Last Name</FormLabel>
-                    <Input name="lastName" type="text" placeholder="Last Name" onChange={handleChange}/>
-                </FormControl>
-                <FormControl id="email" isRequired>
-                    <FormLabel>Email</FormLabel>
-                    <Input name="email" type="text" placeholder="Email" onChange={handleChange}/>
-                </FormControl>
-                <FormControl id="password" isRequired>
-                    <FormLabel>Password</FormLabel>
-                    <Input name="password" type="password" placeholder="Password" onChange={handleChange}/>
-                </FormControl>
-                <Button mt={4} type="submit" colorScheme="teal">
-                    Register
-                </Button>
-            </form>
-            <Button mt={4} onClick={handleGoogleLogin} colorScheme="red">
-                Register with Google
-            </Button>
-            <Button mt={4} onClick={navigateToLogin} colorScheme="blue">
-                Already have an account? Login!
-            </Button>
-        </Box>
-    )
+        <Flex
+            minH={'100vh'}
+            align={'center'}
+            justify={'center'}
+            bg={'gray.800'}>
+            <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
+                <Stack align={'center'}>
+                    <Heading fontSize={'4xl'} color={'white'}>
+                        Create an account
+                    </Heading>
+                </Stack>
+                <Box
+                    rounded={'lg'}
+                    bg={'gray.700'}
+                    boxShadow={'lg'}
+                    p={8}>
+                    {error && (
+                        <Alert status="error">
+                            <AlertIcon />
+                            {error}
+                        </Alert>
+                    )}
+                    <form onSubmit={handleSubmit}>
+                        <Stack spacing={4}>
+                            <FormControl id="firstName" isRequired>
+                                <FormLabel color={'white'}>First Name</FormLabel>
+                                <Input name="firstName" type="text" onChange={handleChange} />
+                            </FormControl>
+                            <FormControl id="lastName" isRequired>
+                                <FormLabel color={'white'}>Last Name</FormLabel>
+                                <Input name="lastName" type="text" onChange={handleChange} />
+                            </FormControl>
+                            <FormControl id="email" isRequired>
+                                <FormLabel color={'white'}>Email</FormLabel>
+                                <Input name="email" type="text" onChange={handleChange} />
+                            </FormControl>
+                            <FormControl id="password" isRequired>
+                                <FormLabel color={'white'}>Password</FormLabel>
+                                <Input name="password" type="password" onChange={handleChange} />
+                            </FormControl>
+                            <Stack spacing={2}>
+                                <Button
+                                    bg={'blue.600'}
+                                    color={'white'}
+                                    _hover={{
+                                        bg: 'blue.700',
+                                    }}
+                                    type="submit"
+                                >
+                                    Register
+                                </Button>
+                                <Text align='center' color={'white'}>
+                                    or
+                                </Text>
+                                <Button
+                                    bg={'red.600'}
+                                    color={'white'}
+                                    _hover={{
+                                        bg: 'red.700',
+                                    }}
+                                    onClick={handleGoogleLogin}
+                                >
+                                    Register with Google
+                                </Button>
+                            </Stack>
+                        </Stack>
+                    </form>
+                    <Flex justify="center" mt={4}>
+                        <Link onClick={navigateToLogin} fontSize='sm' color={'blue.400'}>
+                            Already have an account? Login!
+                        </Link>
+                    </Flex>
+                </Box>
+            </Stack>
+        </Flex>
+    );
 }
-
-export default Register;
-
-
-// https://reactrouter.com/en/main/hooks/use-navigate
