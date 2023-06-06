f19601a28d106dcd4af839b3d599ce6a9dd880d8
duffy <dufei353194284@gmail.com>
2023-06-05 22:59:19 -0400

    commiting to rebase

--- server/api/openAI.js
+++ server/api/openAI.js
@@ -12,3 +12,23 @@ async function listModels() {
   const response = await openai.listModels();
   console.log(response);
 }
+
+// example call to use a fine-tuned model
+async function getTrainedModelResponse(model, prompt) {
+    const response = await openai.createCompletion({
+        model: model,
+        prompt: prompt
+      });
+    console.log(response);
+}
+
+// example call to delete a fine-tuned model (must be the designated owner in the organization)
+async function deleteTrainedModel(model) {
+    const response = await openai.deleteModel({
+        model: model
+      });
+    console.log(response);
+}
+
+// openAI training data file upload (JSONL)
+
