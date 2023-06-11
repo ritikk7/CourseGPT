83397a0088c45998d5c621a6044a81d2647fad28
Amy Jo <amyjojjm2001@gmail.com>
2023-06-10 17:30:12 -0700

    backend in progress

--- server/controllers/chat.js
+++ server/controllers/chat.js
@@ -1,3 +1,5 @@
+const Chat = require('../models/chat');
+
 async function getChat(req, res) {
   // TODO
   const userId = req.params.userId;
@@ -9,6 +11,17 @@ async function createChat(req, res) {
   // TODO
   const userId = req.params.userId;
   res.send({ data: `Hello create new chat for user ${userId}` });
+  try {
+    const chat = new Chat({
+      users: userId,
+      messages: [],
+      course: req.body.course,
+    });
+    await chat.save();
+    res.status(201).send({ chat: chat });
+  } catch (error) {
+    res.status(500).send({ error: error.message });
+  }
 }
 
 async function updateChat(req, res) {
@@ -16,6 +29,21 @@ async function updateChat(req, res) {
   const userId = req.params.userId;
   const chatId = req.params.chatId;
   res.send({ data: `Hello update chatId ${chatId} for user ${userId}` });
+
+  try {
+    const chat = await Chat.findById(chatId);
+    if (!chat) {
+      res.status(404).send({ error: 'Chat not found' });
+    }
+    const { messages, course, deleted } = req.body;
+    chat.messages = messages;
+    chat.course = course;
+    chat.deleted = deleted;
+    const updatedChat = await chat.save();
+    res.status(200).send({ updatedChat: updatedChat });
+  } catch (error) {
+    res.status(500).send({ error: error.message });
+  }
 }
 
 async function deleteChat(req, res) {
@@ -23,6 +51,17 @@ async function deleteChat(req, res) {
   const userId = req.params.userId;
   const chatId = req.params.chatId;
   res.send({ data: `Hello delete chatId ${chatId} for user ${userId}` });
+
+  try {
+    const chat = await Chat.findById(chatId);
+    if (!chat) {
+      res.status(404).send({ error: 'Chat not found' });
+    }
+    await chat.remove();
+    res.status(200).send({ message: 'Chat deleted successfully' });
+  } catch (error) {
+    res.status(500).send({ error: error.message });
+  }
 }
 
 module.exports = {
