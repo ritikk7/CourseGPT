5572bfa755c0094cc4d2d3a0229b658456578bc7
Amy Jo <amyjojjm2001@gmail.com>
2023-06-20 18:05:29 -0700

    mui icon

--- client/src/components/molecules/ExistingChat/ExistingChat.js
+++ client/src/components/molecules/ExistingChat/ExistingChat.js
@@ -4,18 +4,19 @@ import { ChatIcon } from '@chakra-ui/icons';
 import styles from './ExistingChat.module.css';
 import { Button, Text } from '@chakra-ui/react';
 import { useSelector } from 'react-redux';
-
+import DeleteIcon from '@mui/icons-material/Delete';
 const ExistingChat = ({ title, handleExistingChatClick, id }) => {
   const focusedChat = useSelector(state => state.chats.focusedChat);
   const [isFocused, setIsFocused] = useState(false);
+  const maxCharLength = 21;
 
   useEffect(() => {
     setIsFocused(focusedChat == id);
   }, [focusedChat]);
 
   const truncateTitle = () => {
-    if (title.length > 20) {
-      return title.slice(0, 20) + '...';
+    if (title.length > maxCharLength) {
+      return title.slice(0, maxCharLength) + '...';
     }
     return title;
   };
@@ -24,8 +25,9 @@ const ExistingChat = ({ title, handleExistingChatClick, id }) => {
     <Button
       width="100%"
       bg={isFocused ? 'rgb(61, 61, 61)' : 'transparent'}
-      _hover={{ bg: 'rgb(61, 61, 61)' }}
+      _hover={isFocused ? { bg: 'rgb(61, 61, 61)' } : { bg: 'rgb(47, 47, 47)' }}
       onClick={() => handleExistingChatClick(id)}
+      pl={3}
     >
       <div className={styles.existingChat}>
         <div>
@@ -34,6 +36,17 @@ const ExistingChat = ({ title, handleExistingChatClick, id }) => {
         <Text fontSize="sm" fontWeight="normal" pl={2} my="auto">
           {truncateTitle(title)}
         </Text>
+        {isFocused && (
+          <Button
+            bg="rgb(61, 61, 61)"
+            _hover={{ bg: 'rgb(61, 61, 61)' }}
+            position="absolute"
+            right={-2}
+            top={-0.5}
+          >
+            <DeleteIcon fontSize="small" />
+          </Button>
+        )}
       </div>
     </Button>
   );
